d3e56d06452500146c6c1a6681f060925af4616a
YahefuYusufu <parguzatt@gmail.com>
2024-09-10 11:39:00 +0200

    work with the sideBar

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,21 @@
+import { useEffect, useState } from "react"
+import { Route, Routes, useLocation } from "react-router-dom"
+import Loader from "./components/common"
+
 import Dashboard from "./pages/Dashboard"
 
 function App() {
+	const [loading, setLoading] = useState<boolean>(true)
+	const { pathname } = useLocation()
+
+	useEffect(() => {
+		window.scrollTo(0, 0)
+	}, [pathname])
+
+	useEffect(() => {
+		setTimeout(() => setLoading(false), 1000)
+	}, [])
+
 	return (
 		<>
 			<Dashboard />
--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,35 +1,49 @@
 import React from "react"
-import { Home, Calendar, PieChart, Bell, Settings, Menu } from "react-feather"
+import {
+	Home,
+	Calendar,
+	PieChart,
+	Bell,
+	Settings,
+	AlignLeft,
+} from "react-feather"
 
-const Drawer: React.FC = () => {
+interface SidebarProps {
+	sidebarOpen: boolean
+	setSidebarOpen: (arg: boolean) => void
+}
+
+const Sidebar = ({ sidebarOpen, setSidebarOpen }: SidebarProps) => {
 	return (
-		<div className="fixed top-0 left-0 h-full w-32 bg-gray-200  ">
-			<div className="">
-				<li className="p-2 flex">
-					<Menu size={24} />
-					<a href="/hunburger" />
-				</li>
+		<div className="fixed top-0 left-0 h-full w-24 bg-gray-200  ">
+			<div>
+				<ul className="p-4">
+					<li className="p-4 flex item-center justify-between indigo-700">
+						<AlignLeft size={22} color="#51459E" />
+						<a href="/hunburger" />
+					</li>
+				</ul>
 			</div>
 			<div>
 				<ul className="p-4">
-					<li className="p-2 flex items-center">
-						<Home size={24} />
+					<li className="p-4 flex items-center">
+						<Home size={20} />
 						<a href="/home" />
 					</li>
-					<li className="p-2 flex items-center">
-						<Calendar size={24} />
+					<li className="p-4 flex items-center">
+						<Calendar size={20} color="#030229" />
 						<a href="/calendar" />
 					</li>
-					<li className="p-2 flex items-center">
-						<PieChart size={24} />
+					<li className="p-4 flex items-center">
+						<PieChart size={20} color="#030229" />
 						<a href="/analysis" />
 					</li>
-					<li className="p-2 flex items-center">
-						<Bell size={24} />
+					<li className="p-4 flex items-center">
+						<Bell size={20} color="#030229" />
 						<a href="/notification" />
 					</li>
-					<li className="p-2 flex items-center">
-						<Settings size={24} />
+					<li className="p-4 flex items-center">
+						<Settings size={20} color="#030229" />
 						<a href="/settings" />
 					</li>
 				</ul>
@@ -38,4 +52,4 @@ const Drawer: React.FC = () => {
 	)
 }
 
-export default Drawer
+export default Sidebar
